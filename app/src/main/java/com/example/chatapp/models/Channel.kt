f2840d490fcd2a7c18f3d07e584092b20da33fa9
mirerakepha <kephamirera16@gmail.com>
2025-10-01 12:42:14 +0300

    fix firebase realtime database

--- app/src/main/java/com/example/chatapp/models/Channel.kt
+++ app/src/main/java/com/example/chatapp/models/Channel.kt
@@ -3,9 +3,5 @@ package com.example.chatapp.models
 data class Channel(
     val id: String = "",
     val name: String = "",
-    val email: String = "",
     val createdAt: Long = System.currentTimeMillis()
-){
-    // Add empty constructor for Firebase
-    constructor() : this("", "")
-}
+)
